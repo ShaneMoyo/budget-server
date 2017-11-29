5d9a5182cc746e7c1aa566c583bb522dfd2d91d5
Shane Moyo <smoyo@pdx.edu>
2017-11-29 11:25:10 -0800

    passing buget save test

--- test/e2e/budgets.test.js
+++ test/e2e/budgets.test.js
@@ -5,11 +5,11 @@ const assert = chai.assert;
 
 
 describe('budget API', () => {
-    beforeEach(() => mongoose.Connection.dropDatabase());
+    beforeEach(() => mongoose.connection.dropDatabase());
     
     const testBudget = {
         name: 'Test Budget',
-        name: 9999
+        budget: 9999
     };
 
     it('Should save a budget with id', () => {
