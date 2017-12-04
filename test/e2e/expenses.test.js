1af7f4708c0730d9a4bbf7bb650b7cfb2edb0c18
Shane Moyo <smoyo@pdx.edu>
2017-12-04 10:50:01 -0800

    added failing get by budget id test

--- test/e2e/expenses.test.js
+++ test/e2e/expenses.test.js
@@ -9,6 +9,7 @@ describe('Expense API', () => {
     beforeEach(()=> mongoose.connection.dropDatabase());
      
     let savedBudget;
+    let testExpenses;
 
     beforeEach(()=>{
         const testBudget = {
@@ -19,24 +20,53 @@ describe('Expense API', () => {
             .send(testBudget)
             .then(({ body })=> {
                 savedBudget = body;
+                testExpenses = [
+                    {
+                        name: 'Test Expense 1',
+                        amount: 20,
+                        budget: savedBudget._id
+                    },
+                    {
+                        name: 'Test Expense 2',
+                        amount: 333,
+                        budget: savedBudget._id
+                    },
+                    {
+                        name: 'Test Expense 3',
+                        amount: 290,
+                        budget: savedBudget._id
+                    }
+                ]
             });
     })
 
     it('Shoud save an expense', () => {
-        const testExpense = {
-            name: 'Test Expense',
-            amount: 20,
-            budget: savedBudget._id
-        }
-
         return request.post('/api/expenses')
-            .send(testExpense)
+            .send(testExpenses[0])
             .then(({ body })=> {
                 const savedExpense = body; 
                 assert.ok(savedExpense._id);
-                assert.equal(savedExpense.name, testExpense.name)
+                assert.equal(savedExpense.name, testExpenses[0].name)
             });
     });
+
+    it('should get all expenses of parent budget', () => {
+        const saveExpenses = testExpenses.map( expense => {
+            return request.post('/api/expenses')
+                .send(expense)
+                .then( res => res.body );
+        });
+
+        return Promise.all(savedExpenses)
+            .then( savedExpenses => {
+                return request.get('api/expenses/:id')
+                    .then( gotExpeneses =>  {
+                        gotExpeneses = gotExpeneses.sort((a, b) => a._id < b._id);
+                        savedExpenses = savedExpenses.sort((a, b) => a._id < b._id);
+                        assert.seepEqual(gotExpeneses, savedExpenses);
+                    });
+            })
+    })
 
 
 })
