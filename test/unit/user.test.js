ac5d2c52f6627aa88787ef405ff9fc236cfc60e6
Shane Moyo <smoyo@pdx.edu>
2017-12-08 08:19:45 -0800

    added hash test

---
+++ test/unit/user.test.js
@@ -0,0 +1,16 @@
+const assert = require('chai').assert;
+const User = require('../../lib/models/user');
+
+describe('user model', () => {
+    const testUser = new User({
+        email: 'test@test.com'
+    });
+
+    const password = 'XYZ';
+
+    it('generates hash from password', () => {
+        testUser.generateHash(password);
+        assert.isOk(testUser.hash);
+        assert.notEqual(testUser.hash, password);
+    });
+})
